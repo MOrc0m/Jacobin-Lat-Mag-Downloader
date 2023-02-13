82db12b1c762a9a44bb51116b334501de6ecc4d9
Nicolas Deleville <veranodel92@hotmail.com>
2023-02-13 15:53:50 -0300

    Update JacobinDownloader.py
    Added spanish comments

--- JacobinDownloader.py
+++ JacobinDownloader.py
@@ -2,35 +2,40 @@
 import os
 import requests
 
-# saving url constants for magazine's url
+# saving url constants for magazine's url / partes fijas de la url
 
 URL_1='https://jacobinlat.com/wp-content/uploads'
 URL_2='revista-Jacobin_No'
 URL_3='_ed-digital.pdf'
 
-# User's input for url's variable parts
+# User's input for url's variable parts / input de usuario para partes variables de la url
 
 YEAR=input("ingrese año de edicion / input edition's year: ")
 MONTH=input("ingrese mes de edicion / input edition's  month: ")
 NMB=input("Ingrese numero de edicion/ input edition's number: ")
 
-# String concatenation for url crafting 
+# String concatenation for url crafting / concatenacion para formar la url final.
 
 URL= URL_1 + '/' + YEAR + '/' + MONTH + '/' + URL_2 + NMB + URL_3
 print('URL:', URL)
 
-# Requesting the crafted url
+# Requesting the crafted url / peticion http de la url obtenida
 
 R=requests.get(URL)
 CODE=R.status_code
 
-#Write the directory  you want to save at
+# Write the directory  you want to save at / Eleccion de directorio de descarga
 
 DIR="" 
 print("Directorio de guardado/Save directory: " + DIR)
 
-#If http status code = 200 the url is valid and we proceed to search or create the save directory, then we download the magazine. If status code is not 200 then the url is not valid and we throw an error message.
-    
+# If http status code = 200 the url is valid and we proceed to search or create the save directory 
+# Si el codigo de estado http es 200, la url es valida y procedemos a buscar o crear el directorio de descarga
+# then we download the magazine. 
+# Luego descargamos la revista
+# If status code is not 200 then the url is not valid and we throw an error message.
+# Si el codigo de estado no es 200, entonces la url no es valida y mostramos un mensaje de error
+
 if CODE == 200: 
     if not os.path.exists(DIR):
         os.makedirs(DIR)
